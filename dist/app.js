2053378813cf3fa2f6ebfa27bbb333efe5d049a8
Arran <arranstrange@googlemail.com>
2025-04-25 13:59:15 +0100

    Revert "chore: add built files for App Runner deploy"
    This reverts commit 059914052c969ba0db7b1f736ebe244db322e10e.

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -14,22 +23,24 @@ const resolvers_1 = __importDefault(require("./graphQL/resolvers"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
-async function startServer() {
-    const server = new apollo_server_express_1.ApolloServer({
-        typeDefs: schema_1.default,
-        resolvers: resolvers_1.default,
-    });
-    await server.start();
-    server.applyMiddleware({ app });
-    const PORT = process.env.PORT || 4000;
-    try {
-        await mongoose_1.default.connect(process.env.MONGO_URI);
-        console.log("Connected to MongoDB");
-        app.listen(PORT, () => {
-            console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+function startServer() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const server = new apollo_server_express_1.ApolloServer({
+            typeDefs: schema_1.default,
+            resolvers: resolvers_1.default,
         });
-    }
-    catch (err) {
-        console.error("DB connection failed", err);
-    }
+        yield server.start();
+        server.applyMiddleware({ app });
+        const PORT = process.env.PORT || 4000;
+        try {
+            yield mongoose_1.default.connect(process.env.MONGO_URI);
+            console.log("Connected to MongoDB");
+            app.listen(PORT, () => {
+                console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+            });
+        }
+        catch (err) {
+            console.error("DB connection failed", err);
+        }
+    });
 }
--- dist/graphQL/resolvers.js
+++ dist/graphQL/resolvers.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,38 +15,38 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Run_1 = __importDefault(require("../models/Run"));
 const resolvers = {
     Query: {
-        runs: async () => Run_1.default.find(),
-        run: async (_, { id }) => {
+        runs: () => __awaiter(void 0, void 0, void 0, function* () { return Run_1.default.find(); }),
+        run: (_1, _a) => __awaiter(void 0, [_1, _a], void 0, function* (_, { id }) {
             console.log("Fetching run with ID:", id);
-            return await Run_1.default.findById(id);
-        },
+            return yield Run_1.default.findById(id);
+        }),
     },
     Mutation: {
-        startRun: async () => {
+        startRun: () => __awaiter(void 0, void 0, void 0, function* () {
             const run = new Run_1.default({
                 startTime: new Date().toISOString(),
                 coordinates: [],
             });
-            return await run.save();
-        },
-        stopRun: async (_, { runId, name, endTime, distance, coordinates }) => {
-            const run = await Run_1.default.findById(runId);
+            return yield run.save();
+        }),
+        stopRun: (_1, _a) => __awaiter(void 0, [_1, _a], void 0, function* (_, { runId, name, endTime, distance, coordinates }) {
+            const run = yield Run_1.default.findById(runId);
             if (!run)
                 throw new Error("Run not found");
             const startTime = new Date(run.startTime).getTime();
             const end = new Date(endTime).getTime();
             const totalTime = Math.floor((end - startTime) / 1000);
-            return await Run_1.default.findByIdAndUpdate(runId, {
+            return yield Run_1.default.findByIdAndUpdate(runId, {
                 name,
                 endTime,
                 totalTime,
                 distance,
                 coordinates,
             }, { new: true });
-        },
-        deleteRun: async (_, { id }) => {
-            return await Run_1.default.findByIdAndDelete(id);
-        },
+        }),
+        deleteRun: (_1, _a) => __awaiter(void 0, [_1, _a], void 0, function* (_, { id }) {
+            return yield Run_1.default.findByIdAndDelete(id);
+        }),
     },
 };
 exports.default = resolvers;
