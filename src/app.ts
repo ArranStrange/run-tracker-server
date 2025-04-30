d896552e21229415a129a8de052abfcea96916ce
Arran <arranstrange@googlemail.com>
2025-04-30 15:40:30 +0100

    Fix: :( bind port before connecting to MongoDB

--- src/app.ts
+++ src/app.ts
@@ -19,24 +19,20 @@ export async function startServer() {
   await server.start();
   server.applyMiddleware({ app });
 
+  const PORT = process.env.PORT || 4000;
+
+  app.get("/", (req, res) => {
+    res.send("🚀 Server is running");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Server listening on port ${PORT}`);
+  });
+
   try {
     await mongoose.connect(process.env.MONGO_URI!);
     console.log("✅ Connected to MongoDB");
-
-    app.get("/", (req, res) => {
-      res.send("🚀 Server is running");
-    });
-
-    const PORT = process.env.PORT || 4000;
-
-    app.listen(PORT, () => {
-      console.log(`🚀 Server listening on port ${PORT}`);
-    });
   } catch (err) {
     console.error("❌ Database connection failed", err);
   }
 }
-
-console.log("✅ Server entry file is executing");
-startServer();
-console.log("✅ Server entry file has finished executing");
